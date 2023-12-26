eb5239d5e61f913905aa36ad36d1d4e0592a039e
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-26 13:20:06 +0200

    finishing Divisor

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,8 @@
 /**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
+ * Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
 	}
 }
