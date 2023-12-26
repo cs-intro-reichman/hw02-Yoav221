d0366646f0b3060e050770ed8904c0b125c3ae65
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-26 14:10:24 +0200

    finishing inorder

--- InOrder.java
+++ InOrder.java
@@ -1,9 +1,23 @@
 /**
- *  Generates and prints random integers in the range [0,10),
- *  as long as they form a non-decreasing sequence.
+ * Generates and prints random integers in the range [0,10),
+ * as long as they form a non-decreasing sequence.
  */
 public class InOrder {
-	public static void main (String[] args) {
-		//// Write your code here
+	public static void main(String[] args) {
+		int c = 0;
+
+		while (true) {
+			int randomNumber = (int) (Math.random() * 10);
+
+			if (randomNumber > c) {
+				System.out.print(randomNumber + "  ");
+				c = randomNumber;
+
+			} else {
+				break;
+			}
+
+		}
+
 	}
 }
