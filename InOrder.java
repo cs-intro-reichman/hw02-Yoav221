fb4ff43cc9fe1fb86c12645d36f3f22c666c333d
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-27 12:49:17 +0200

    fixing inorder

--- InOrder.java
+++ InOrder.java
@@ -9,14 +9,11 @@ public class InOrder {
 		String result = "";
 		while (true) {
 			int randomNumber = (int) (Math.random() * 10);
-			if ((randomNumber > c) && (c == 0)) {
+			if (randomNumber > c) {
 				result = result + randomNumber;
 				c = randomNumber;
 
-			} else if ((randomNumber > c) && (c != 0)) {
-				result = result + randomNumber;
-				c = randomNumber;
-			} else {
+			} else if (randomNumber <= c) {
 				break;
 			}
 
